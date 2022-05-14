a7c9b7d9618c8eeffb123ff32ae1a429320e0e6f
Reynell Quevedo <reynell200501@gmail.com>
2022-05-14 08:48:29 -0500

    Fix DFS

--- src/control/Busqueda.java
+++ src/control/Busqueda.java
@@ -9,7 +9,6 @@ import modelo.Par;
 
 public class Busqueda {
 
-
 	private static int[][] tablero;
 	private static int[][] aux;
 	private static Arbol raiz;
@@ -20,18 +19,20 @@ public class Busqueda {
 	private static int colMeta1;
 	private static int rowMeta2;
 	private static int colMeta2;
-	int [] dirI = {-1, 1, 0, 0};
-	int [] dirJ = {0, 0, -1, 1};
+	int[] dirI = { -1, 1, 0, 0 };
+	int[] dirJ = { 0, 0, -1, 1 };
 	private Par inicio;
-	private Par [] naves;
-	private Par [] items;
+	private Par[] naves;
+	private Par[] items;
+
 	public Busqueda() {
 		naves = new Par[2];
 		items = new Par[2];
 	}
+
 	public void init() {
-		
-		tablero = new int [10][10];
+
+		tablero = new int[10][10];
 
 		try {
 
@@ -43,159 +44,160 @@ public class Busqueda {
 		}
 		int item = 0;
 		System.out.println();
-		for(int i = 0; i < 10; ++i){
-			for(int j = 0; j < 10; ++j){
+		for (int i = 0; i < 10; ++i) {
+			for (int j = 0; j < 10; ++j) {
 				System.out.print(tablero[i][j] + "  ");
-				if ( tablero[i][j] == 2){
-					
-					inicio = new Par (i,j);
-					
-				}else if ( tablero[i][j] == 3){
-					
-					naves[0] = new Par (i,j);
-					
-				}else if ( tablero[i][j] == 4){
-					
-					naves[1] = new Par (i,j);
-					
-				}else if ( tablero[i][j] == 5 ){
-					
-					items[item] = new Par (i,j);
+				if (tablero[i][j] == 2) {
+
+					inicio = new Par(i, j);
+
+				} else if (tablero[i][j] == 3) {
+
+					naves[0] = new Par(i, j);
+
+				} else if (tablero[i][j] == 4) {
+
+					naves[1] = new Par(i, j);
+
+				} else if (tablero[i][j] == 5) {
+
+					items[item] = new Par(i, j);
 					item++;
 				}
 			}
 			System.out.println();
 		}
-		
-		
-		
-		
+
 	}
 
 	public Node BFS() {
 		Queue<Node> pq = new LinkedList<>();
 		pq.add(new Node(inicio, null, 0, new boolean[2], new boolean[2], 0, 0));
-		while(!pq.isEmpty()) {
+		while (!pq.isEmpty()) {
 			Node n = pq.poll();
-			if(!n.hasCycles()) {
+			if (!n.hasCycles()) {
 				estaEnNave(n);
 				estaEnItem(n);
-				if(n.hasFinished())
+				if (n.hasFinished())
 					return n;
-				for(int i = 0 ; i < 4 ; ++i) {
+				for (int i = 0; i < 4; ++i) {
 					Par pos = n.pos;
-					if(isPossible(pos.getI() + dirI[i] , pos.getJ() + dirJ[i])) {
-						int newCost = calcularCosto(pos.getI() + dirI[i] ,  pos.getJ() + dirJ[i] , n.nave) + n.cost;
+					if (isPossible(pos.getI() + dirI[i], pos.getJ() + dirJ[i])) {
+						int newCost = calcularCosto(pos.getI() + dirI[i], pos.getJ() + dirJ[i], n.nave) + n.cost;
 						int gas = 0;
-						if(n.nave != 0) {
-							gas = n.nave -1;
+						if (n.nave != 0) {
+							gas = n.nave - 1;
 						}
-						Node son = new Node(new Par(pos.getI() + dirI[i] , pos.getJ() + dirJ[i]) , n, gas ,
-								n.items.clone() , n.naves.clone() , newCost, 0);
+						Node son = new Node(new Par(pos.getI() + dirI[i], pos.getJ() + dirJ[i]), n, gas,
+								n.items.clone(), n.naves.clone(), newCost, 0);
 						pq.add(son);
 					}
-					
+
 				}
 			}
 		}
-		
+
 		return null;
 	}
+
 	public Node DFS() {
 		Stack<Node> pq = new Stack<>();
 		pq.add(new Node(inicio, null, 0, new boolean[2], new boolean[2], 0, 0));
-		while(!pq.isEmpty()) {
+		while (!pq.isEmpty()) {
 			Node n = pq.pop();
-			if(!n.hasCycles()) {
+			if (!n.hasCycles()) {
 				estaEnNave(n);
 				estaEnItem(n);
-				if(n.hasFinished())
+				if (n.hasFinished())
 					return n;
-				for(int i = 0 ; i < 4 ; ++i) {
+				for (int i = 3; i > -1; --i) {
 					Par pos = n.pos;
-					if(isPossible(pos.getI() + dirI[i] , pos.getJ() + dirJ[i])) {
-						int newCost = calcularCosto(pos.getI() + dirI[i] ,  pos.getJ() + dirJ[i] , n.nave) + n.cost;
+					if (isPossible(pos.getI() + dirI[i], pos.getJ() + dirJ[i])) {
+						int newCost = calcularCosto(pos.getI() + dirI[i], pos.getJ() + dirJ[i], n.nave) + n.cost;
 						int gas = 0;
-						if(n.nave != 0) {
-							gas = n.nave -1;
+						if (n.nave != 0) {
+							gas = n.nave - 1;
 						}
-						Node son = new Node(new Par(pos.getI() + dirI[i] , pos.getJ() + dirJ[i]) , n, gas ,
-								n.items.clone() , n.naves.clone() , newCost, 0);
+						Node son = new Node(new Par(pos.getI() + dirI[i], pos.getJ() + dirJ[i]), n, gas,
+								n.items.clone(), n.naves.clone(), newCost, 0);
 						pq.add(son);
 					}
-					
+
 				}
 			}
 		}
-		
+
 		return null;
 	}
+
 	public Node BCU() {
-		PriorityQueue<Node> pq = new PriorityQueue<>( (a, b) -> a.cost - b.cost );
+		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
 		pq.add(new Node(inicio, null, 0, new boolean[2], new boolean[2], 0, 0));
-		while(!pq.isEmpty()) {
+		while (!pq.isEmpty()) {
 			Node n = pq.poll();
-			if(!n.hasCycles()) {
+			if (!n.hasCycles()) {
 				estaEnNave(n);
 				estaEnItem(n);
-				if(n.hasFinished())
+				if (n.hasFinished())
 					return n;
-				for(int i = 0 ; i < 4 ; ++i) {
+				for (int i = 0; i < 4; ++i) {
 					Par pos = n.pos;
-					if(isPossible(pos.getI() + dirI[i] , pos.getJ() + dirJ[i])) {
-						int newCost = calcularCosto(pos.getI() + dirI[i] ,  pos.getJ() + dirJ[i] , n.nave) + n.cost;
+					if (isPossible(pos.getI() + dirI[i], pos.getJ() + dirJ[i])) {
+						int newCost = calcularCosto(pos.getI() + dirI[i], pos.getJ() + dirJ[i], n.nave) + n.cost;
 						int gas = 0;
-						if(n.nave != 0) {
-							gas = n.nave -1;
+						if (n.nave != 0) {
+							gas = n.nave - 1;
 						}
-						Node son = new Node(new Par(pos.getI() + dirI[i] , pos.getJ() + dirJ[i]) , n, gas ,
-								n.items.clone() , n.naves.clone() , newCost, 0);
+						Node son = new Node(new Par(pos.getI() + dirI[i], pos.getJ() + dirJ[i]), n, gas,
+								n.items.clone(), n.naves.clone(), newCost, 0);
 						pq.add(son);
 					}
-					
+
 				}
 			}
 		}
-		
+
 		return null;
 	}
+
 	public int calcularCosto(int i, int j, int gas) {
-		if(gas > 0 ) {
+		if (gas > 0) {
 			return 1;
-		}else {
-			if(tablero[i][j] == 6) {
+		} else {
+			if (tablero[i][j] == 6) {
 				return 4;
-			}else {
+			} else {
 				return 1;
 			}
-			
+
 		}
 	}
+
 	public void estaEnNave(Node n) {
 		Par pos = n.pos;
-		for(int i = 0 ; i < 2 ; ++i) {
-			if(pos.getI() == naves[i].getI() && pos.getJ() == naves[i].getJ() && !n.naves[i] ) {
+		for (int i = 0; i < 2; ++i) {
+			if (pos.getI() == naves[i].getI() && pos.getJ() == naves[i].getJ() && !n.naves[i]) {
 				n.naves[i] = true;
-				if(i == 0 ) {
+				if (i == 0) {
 					n.nave = 10;
-				}else {
+				} else {
 					n.nave = 20;
 				}
 			}
 		}
 	}
+
 	public void estaEnItem(Node n) {
 		Par pos = n.pos;
-		for(int i = 0 ; i < 2 ; ++i) {
-			if(pos.getI() == items[i].getI() && pos.getJ() == items[i].getJ() && !n.items[i] ) {
+		for (int i = 0; i < 2; ++i) {
+			if (pos.getI() == items[i].getI() && pos.getJ() == items[i].getJ() && !n.items[i]) {
 				n.items[i] = true;
-				
+
 			}
 		}
 	}
-	
-	
+
 	public boolean isPossible(int i, int j) {
-		return !(i < 0 || j < 0 || i >= 10 || j >= 10 || tablero[i][j]==1);
+		return !(i < 0 || j < 0 || i >= 10 || j >= 10 || tablero[i][j] == 1);
 	}
 }
